3289acbe0896b726ce4928a541b8c8355e0d18f0
JoshFransix <joshiandersonk69@gmail.com>
2024-09-05 21:26:50 +0100

    updated parameter type for render deploy

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { v4 as uuidv4 } from "uuid";
 
-export async function POST(response: NextResponse) {
-  const data = await response.json();
+export async function POST(request: NextRequest) {
+  const data = await request.json();
 
   return NextResponse.json({ id: uuidv4(), ...data });
 }
