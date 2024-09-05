91e29f9c4618f6c7f8d963bde0ca7f9d570c97f4
JoshFransix <joshiandersonk69@gmail.com>
2024-09-05 21:36:03 +0100

    feat: update uuid usage

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
+const { v4 } = require("uuid");
 
 export async function POST(request: NextRequest) {
   const data = await request.json();
 
-  return NextResponse.json({ id: uuidv4(), ...data });
+  return NextResponse.json({ id: v4(), ...data });
 }
